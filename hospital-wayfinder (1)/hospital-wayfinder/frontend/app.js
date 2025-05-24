77cd040237dff7bdf21fd3698583921d3e15eacc
Bradw212 <brad.watson212@gmail.com>
2025-05-24 22:17:41 +0100

    Delete hospital-wayfinder (1)/hospital-wayfinder directory

--- hospital-wayfinder (1)/hospital-wayfinder/frontend/app.js
+++
@@ -1,5 +0,0 @@
-function route() {
-  const origin = document.getElementById('origin').value;
-  const dest = document.getElementById('destination').value;
-  alert(`Routing from ${origin} to ${dest}`); // Replace with actual logic
-}
